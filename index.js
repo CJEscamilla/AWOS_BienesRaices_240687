a640b60c88d2096df1f92fd4426ab16fed505119
Cristopher Joseph Escamilla Vargas <240687@utxicotepec.edu.mx>
2026-02-16 16:24:54 -0600

    PC-Bienes - Sección03
    add: se crearon nuevas carpetas y archivos

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-//console.log("Hola desde JS");
-import express from 'express'
-import usuarioRoutes from './routes/usuarioRoutes.js'
-// Crea una instancia del contenedor web 
+import express, { json } from "express";
+import usuarioRouter from "./routes/usuarioRouter.js";
+
 const app = express();
 const PORT = process.env.PORT ?? 4000;
 
-app.use("/", usuarioRoutes)
+app.set("view engine", "pug");
+app.set("views", "./views")
 
-app.listen(PORT, ()=> {
-    console.log(`El servidor esta iniciado en el puerto ${PORT}`)
-})
+app.use(express.static(`public`))
 
+app.use("/auth", usuarioRouter);
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
